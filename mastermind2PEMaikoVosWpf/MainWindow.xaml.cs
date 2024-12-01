db30634c04e00b8d17e88012f68b2c50d33ae9c8
MaikoVosPXL <12401562@student.pxl.be>
2024-12-01 15:43:41 +0100

    Mastermind-11

--- mastermind2PEMaikoVosWpf/MainWindow.xaml.cs
+++ mastermind2PEMaikoVosWpf/MainWindow.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Threading;
 
 namespace mastermind2PEMaikoVosWpf
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        TimeSpan elapsedTime;
+        DateTime clicked;
         Random rnd = new Random();
         ComboBox[] guess = new ComboBox[4];
         Ellipse[] selectedEllipse = new Ellipse[4];
@@ -35,12 +35,43 @@ namespace mastermind2PEMaikoVosWpf
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// Start het hoofdvenster en stelt het spel en de timer in bij het laden van het venster.
+        /// </summary>
+        /// <param name="sender"> Het laad-event wordt geactiveerd door het hoofdvenster.</param>
+        /// <param name="e"> De gegevens van het event.</param>
         private void MainWindowLoader(object sender, RoutedEventArgs e)
         {
             StartingGame();
+            clicked = DateTime.Now;
+            timer.Interval = TimeSpan.FromMilliseconds(1);
+            timer.Tick += Timer_Tick;
+            //timer.Start();
         }
+        /// <summary>
+        /// Behandelt de tick van de timer, om de verstreken tijd en pogingen bij te werken.
+        /// </summary>
+        /// <param name="sender"> Het object dat de tick-event heeft geactiveerd.</param>
+        /// <param name="e"> De gegevens van het event.</param>
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            elapsedTime = DateTime.Now - clicked;
+            timerText.Text = $"Timer: {elapsedTime.TotalSeconds.ToString("N2")} / 10";
 
+            if (elapsedTime.TotalSeconds >= 10)
+            {
+                timer.Stop();
+                attempts++;
+                clicked = DateTime.Now;
+                timer.Start();
+            }
+            totalAttempts.Content = $"Attempts: {attempts}/10";
+        }
+        /// <summary>
+        /// Schakelt debugfunctie in of uit op basis van specifieke toetscombinaties.
+        /// </summary>
+        /// <param name="sender">Het object dat het evenement heeft geactiveerd.</param>
+        /// <param name="e">De gegevens van het toetsenbord en de ingedrukte toetsen.</param>
         private void ToggleDebug(object sender, KeyEventArgs e)
         {
             if (e.KeyboardDevice.Modifiers == ModifierKeys.Control && e.Key == Key.F12)
@@ -52,8 +83,20 @@ namespace mastermind2PEMaikoVosWpf
             {
                 showRandomColors.Visibility = Visibility.Hidden;
             }
-        }
 
+            if (e.KeyboardDevice.Modifiers == ModifierKeys.Control && e.Key == Key.F2)
+            {
+                timerText.Visibility = Visibility.Visible;
+                timerText.Text = $"Timer: {elapsedTime.TotalSeconds.ToString("N3")} / 10";
+            }
+            else
+            {
+                timerText.Visibility = Visibility.Hidden;
+            }
+        }
+        /// <summary>
+        /// Start het spel door willekeurige kleuren te genereren, de oplossing vast te leggen en de spelstatus bij te werken.
+        /// </summary>
         private void StartingGame()
         {
             randomNumberColor[0] = PickingRandomColor(rnd.Next(0, 6));
@@ -66,7 +109,11 @@ namespace mastermind2PEMaikoVosWpf
             totalAttempts.Content = $"Attempts: {attempts}/10";
             showRandomColors.Text = randomColorSolution;
         }
-
+        /// <summary>
+        /// Aan de hand van de index word er een string aan de randomNumber gekoppelt.
+        /// </summary>
+        /// <param name="randomNumber"> Een random getal dat de kleur bepaalt.</param>
+        /// <returns> Een string die de naam van de kleur beschrijft.</returns>
         private string PickingRandomColor(int randomNumber)
         {
             switch (randomNumber)
@@ -87,7 +134,11 @@ namespace mastermind2PEMaikoVosWpf
                     return "Black";
             }
         }
-
+        /// <summary>
+        /// Verandert de Ellipse background/fill aan de hand van de geselecteerde index van de combobox
+        /// </summary>
+        /// <param name="sender"> De Combobox waarvan de index wordt geselecteerd.</param>
+        /// <param name="e"> De selectie van de Combobox.</param>
         private void ColorChange(object sender, EventArgs e)
         {
             ComboBox changedComboBox = sender as ComboBox;
@@ -109,7 +160,11 @@ namespace mastermind2PEMaikoVosWpf
                 colorFieldFour.Fill = Colorindex(changedComboBox.SelectedIndex);
             }
         }
-
+        /// <summary>
+        /// Checkt aan de hand van de index van de Combobox welke Brushes deze returns.
+        /// </summary>
+        /// <param name="selectedindex"> De geselecteerde index.</param>
+        /// <returns> Brushes aan de hand van de geselecteerde index.</returns>
         private Brush Colorindex(int selectedindex)
         {
             switch (selectedindex)
@@ -130,8 +185,14 @@ namespace mastermind2PEMaikoVosWpf
                     return null;
             }
         }
-
-        private void LabelColorCheck(Ellipse colorChecker, string[] randomNumberColor, int position, ComboBox input)
+        /// <summary>
+        /// Checkt de invoer van de gebruiker ten opzichte van de oplossing op een specifieke positie.
+        /// </summary>
+        /// <param name="colorChecker"> De Ellipse dat feedback geeft voor de kleurcontrole.</param>
+        /// <param name="randomNumberColor"> Een array van strings met de oplossing.</param>
+        /// <param name="position"> De index van de array met de oplossing.</param>
+        /// <param name="input">De Combobox waarvan de text vergeleken wordt met de oplossing.</param>
+        private void EllipseColorCheck(Ellipse colorChecker, string[] randomNumberColor, int position, ComboBox input)
         {
             string solution;
             switch (position)
@@ -175,7 +236,9 @@ namespace mastermind2PEMaikoVosWpf
                 MessageBox.Show("Invalid color", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
-
+        /// <summary>
+        /// Create nieuwe rows en plaatst de Ellipse in deze rows.
+        /// </summary>
         private void ShowGuess()
         {
             RowDefinition newRow = new RowDefinition();
@@ -209,13 +272,17 @@ namespace mastermind2PEMaikoVosWpf
             }
             rows++;
         }
-
+        /// <summary>
+        /// Checkt de code bij iedere click van de knop.
+        /// </summary>
+        /// <param name="sender">Is de Button knop</param>
+        /// <param name="e">Is de Button click event</param>
         private void CheckCodeButton_Click(object sender, RoutedEventArgs e)
         {
-            LabelColorCheck(colorFieldOne, randomNumberColor, 0, colorOneComboBox);
-            LabelColorCheck(colorFieldTwo, randomNumberColor, 1, colorTwoComboBox);
-            LabelColorCheck(colorFieldThree, randomNumberColor, 2, colorThreeComboBox);
-            LabelColorCheck(colorFieldFour, randomNumberColor, 3, colorFourComboBox);
+            EllipseColorCheck(colorFieldOne, randomNumberColor, 0, colorOneComboBox);
+            EllipseColorCheck(colorFieldTwo, randomNumberColor, 1, colorTwoComboBox);
+            EllipseColorCheck(colorFieldThree, randomNumberColor, 2, colorThreeComboBox);
+            EllipseColorCheck(colorFieldFour, randomNumberColor, 3, colorFourComboBox);
 
             ShowGuess();
             attempts++;
@@ -224,7 +291,9 @@ namespace mastermind2PEMaikoVosWpf
             CheckingAttempts();
             CheckingIfWonGame();
         }
-
+        /// <summary>
+        /// Checkt of de attempts niet overschreden zijn.
+        /// </summary>
         private void CheckingAttempts()
         {
             if (attempts >= 10 && !CheckingIfWonGame())
@@ -236,7 +305,7 @@ namespace mastermind2PEMaikoVosWpf
                     points = 100;
                     totalAttempts.Content = $"Attempts: {attempts}/10";
                     addRows.Children.Clear();
-                    ClearingOutLabels();
+                    ClearingOutMainEllipse();
                     StartingGame();
                 }
                 else
@@ -255,7 +324,7 @@ namespace mastermind2PEMaikoVosWpf
                     totalAttempts.Content = $"Attempts: {attempts}/10";
                     totalScore.Content = $"Score: {points}/100";
                     addRows.Children.Clear();
-                    ClearingOutLabels();
+                    ClearingOutMainEllipse();
                     StartingGame();
                 }
                 else
@@ -266,7 +335,10 @@ namespace mastermind2PEMaikoVosWpf
                 }
             }
         }
-
+        /// <summary>
+        /// CheckingIfWonGame checkt of de vier gekozen Ellipse een DarkRed brush heeft.
+        /// </summary>
+        /// <returns> Als alle Strokes == Brushes.DarkRed dan true, anders false. </returns>
         private bool CheckingIfWonGame()
         {
             selectedEllipse[0] = colorFieldOne;
@@ -286,8 +358,10 @@ namespace mastermind2PEMaikoVosWpf
                 return false;
             }
         }
-
-        private void ClearingOutLabels()
+        /// <summary>
+        /// ClearingOutMainEllipse reset uw hoofd Ellipse on uw Comboboxen terug naar default.
+        /// </summary>
+        private void ClearingOutMainEllipse()
         {
             colorOneComboBox.Text = "";
             colorTwoComboBox.Text = "";
@@ -304,7 +378,12 @@ namespace mastermind2PEMaikoVosWpf
             colorFieldThree.Stroke = default;
             colorFieldFour.Stroke = default;
         }
-
+        /// <summary>
+        /// Bij het vroegtijdig afsluiten komt er een Messagebox met de optie om toch nog door te spelen of het venster toch te sluiten.
+        /// De BypassClosingGame bool wordt alleen gebruikt bij de Failed en de Winner Messagebox om geen herhaling te krijgen voor het afsluiten.
+        /// </summary>
+        /// <param name="sender"> De Window die wordt afgesloten.</param>
+        /// <param name="e"> Is het sluiting van de MainWindow.</param>
         private void ClosingGame(object sender, System.ComponentModel.CancelEventArgs e)
         {
             if (bypassClosingGame)
